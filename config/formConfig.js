5ee2dbdd1019cdfec59e135dc2777fcde0b9fe92
koraphatkc-star <koraphat.kc@gmail.com>
2026-02-07 18:20:59 +0700

    Create config/formConfig.js with dropdown options for ranks, units, missions, and platforms

---
+++ config/formConfig.js
@@ -0,0 +1,35 @@
+const config = {
+  ranks: [
+    'Private',
+    'Corporal',
+    'Sergeant',
+    'Lieutenant',
+    'Captain',
+    'Major',
+    'Colonel',
+    'General'
+  ],
+  units: [
+    'Infantry',
+    'Armor',
+    'Artillery',
+    'Aviation',
+    'Logistics',
+    'Special Forces'
+  ],
+  missions: [
+    'Reconnaissance',
+    'Search and Rescue',
+    'Assault',
+    'Defense',
+    'Support'
+  ],
+  platforms: [
+    'Land',
+    'Air',
+    'Naval',
+    'Space'
+  ]
+};
+
+export default config;
